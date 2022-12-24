bcfbdbfa6084bf8f6d43e4d53940ecc1863fe4ae
Jessica <osalorenzo@gmail.com>
2022-12-23 23:22:08 -0500

    more packages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{self, error::HttpError};
+use async_std;
+use mongodb::{error::Error, options::ClientOptions, Client};
+use redis;
+
+struct DbConn {}
+
+async fn db_connect() -> Result<(), Error> {
+    let mongo_options = ClientOptions::parse("mongodb://0.0.0.0:27017").await?;
+    let client = Client::with_options(mongo_options)?;
+    Ok(())
 }
+fn main() {}
